0b4363f4a2eb55151e99fa4c1acb743bffe87241
ADee <adityaarawat121@gmail.com>
2025-05-17 17:23:44 +0530

    2nd commit

---
+++ app/src/main/java/com/example/stockmarketapp/data/repository/StockRepositoryImpl.kt
@@ -0,0 +1,55 @@
+package com.example.stockmarketapp.data.repository
+
+import com.example.stockmarketapp.data.local.StockDatabase
+import com.example.stockmarketapp.data.mapper.toCompanyListing
+import com.example.stockmarketapp.data.remote.dto.StockApi
+import com.example.stockmarketapp.domain.model.CompanyListing
+import com.example.stockmarketapp.domain.repository.StockRepository
+import com.example.stockmarketapp.util.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import okio.IOException
+import retrofit2.HttpException
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class StockRepositoryImpl @Inject constructor(
+    val api: StockApi,
+    val db: StockDatabase
+): StockRepository{
+
+    private val dao = db.dao
+    override suspend fun getCompanyListing(
+        fetchFromRemote: Boolean,
+        query: String
+    ): Flow<Resource<List<CompanyListing>>> {
+        return flow {
+            emit(Resource.Loading(true))
+            val localListing = dao.searchCompanyListing(query)
+            emit(Resource.Success(
+                data = localListing.map { it.toCompanyListing() }
+            ))
+
+            val isDbEmpty = localListing.isEmpty() && query.isBlank()
+            val shouldJustLoadFromCache = !isDbEmpty && !fetchFromRemote
+            if(shouldJustLoadFromCache){
+                emit(Resource.Loading(false))
+                return@flow
+            }
+
+            val remoteListings = try {
+                val response = api.getListing()
+                response.byteStream()
+            }catch (e: IOException){
+                e.printStackTrace()
+                emit(Resource.Error("Couldn't load data"))
+            }catch (e: HttpException){
+                e.printStackTrace()
+                emit(Resource.Error("Couldn't load data"))
+            }
+
+        }
+    }
+
+}
